c81f571b6b6ada6017963cec5bbe4996dce338ec
Alex Li <alexanderli5210@gmail.com>
2024-11-18 21:24:38 -0600

    Bro what is wrong

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar"
-import { BrowserRouter, Routes, Route  } from "react-router-dom";
-import Developing from "./pages/Developing"
+import Navbar from "./components/Navbar";
+import Developing from "./pages/Developing";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="relative">
-      <BrowserRouter>
+      <BrowserRouter basename="/alexPortfolio">
         <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Developing" element={<Developing />}/>
-        <Route path="/Contact" element={<Contact />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Developing" element={<Developing />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
